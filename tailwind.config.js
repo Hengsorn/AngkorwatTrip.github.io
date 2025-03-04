65b5995b21a70b50d90831f85aa3597f859f96f7
Hengsorn <hengvisal827@gmail.com>
2025-03-04 15:53:20 +0700

    Angkorwat Tour

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'], // Update paths to match your project structure
+  content: ["./*.html"], // Update paths to match your project structure
   theme: {
     extend: {},
   },
